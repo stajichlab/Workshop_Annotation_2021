e076b4188547e56c9984dc9342a6320038034fa9
hyphaltip <jasonstajich.phd@gmail.com>
2021-07-29 06:10:49 +0300

    Add download steps

---
+++ workshop/data/download.sh
@@ -0,0 +1,3 @@
+curl -O https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/013/396/165/GCA_013396165.1_ASM1339616v1/GCA_013396165.1_ASM1339616v1_genomic.fna.gz
+curl -O https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/013/396/165/GCA_013396165.1_ASM1339616v1/GCA_013396165.1_ASM1339616v1_protein.faa.gz
+curl -O https://ftp.ncbi.nlm.nih.gov/genomes/all/GCA/013/396/165/GCA_013396165.1_ASM1339616v1/GCA_013396165.1_ASM1339616v1_cds_from_genomic.fna.gz
