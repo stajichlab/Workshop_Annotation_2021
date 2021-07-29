3cb1614a89bd90383bd4e64b86877b34a9bc4659
hyphaltip <jasonstajich.phd@gmail.com>
2021-07-29 10:20:02 +0300

    update for other asm

--- workshop/annotation_Afum/pipeline/04_annotate.sh
+++ workshop/annotation_Afum/pipeline/04_annotate.sh
@@ -6,11 +6,11 @@ if [ $SLURM_CPUS_ON_NODE ]; then
 fi
 
 conda activate funannotate
-LOCUSPREF=AFISSF21
+LOCUSPREF=SGAir0713
 INDIR=genomes
-GENOME=A_fumigatus_ISSF_21_Chr7.masked.fasta
+GENOME=A_fumigatus_SGAir0713.masked.fasta
 OUTDIR=annotate/$(basename $GENOME .masked.fasta)
 BUSCODB=ascomycota
-time funannotate annotate --species "Aspergillus fumigatus" --strain "ISSF_21" --cpus $CPU \
+time funannotate annotate --species "Aspergillus fumigatus" --strain "SGAir0713" --cpus $CPU \
 	 --input  $OUTDIR --name $LOCUSPREF   --busco_db $BUSCODB
 
